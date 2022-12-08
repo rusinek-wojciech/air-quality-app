d40c5075046d7ee73914e820e50462d0f51b8a8f
rusinek-wojciech <59565620+rusinek-wojciech@users.noreply.github.com>
2022-12-08 22:25:05 +0100

    init of project

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import { Provider } from 'react-redux'
+
+import './index.css'
+import { store } from './store/store'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+)
